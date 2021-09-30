c3013741b7203d9fe554b8923789f4cedf7916f2
Marcin Leja <marcinleja5@gmail.com>
2021-09-30 21:13:35 +0200

    UI elements and some outlets

--- KalkulatorKubikowMarcina/ViewController.swift
+++ KalkulatorKubikowMarcina/ViewController.swift
@@ -9,8 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var srednicaTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        srednicaTextField.text = "DUPA"
         // Do any additional setup after loading the view.
     }
 
