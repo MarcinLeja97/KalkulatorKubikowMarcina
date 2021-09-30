a17cf3f073145c757bef6eca9bcfdc80dc5eb3ae
Marcin Leja <marcinleja5@gmail.com>
2021-09-30 22:41:58 +0200

    imageView added

--- KalkulatorKubikowMarcina/ViewController.swift
+++ KalkulatorKubikowMarcina/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var srednicaTextField: UITextField!
+    
+    @IBOutlet weak var dlugoscTextField: UITextField!
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         srednicaTextField.text = "DUPA"
+        dlugoscTextField.text = "BLADA"
+        //label.
+        
         // Do any additional setup after loading the view.
     }
 
